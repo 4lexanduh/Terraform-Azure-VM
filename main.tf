57cce1474bfe80cbaa068ebcc67a123060d2a629
4lexanduh <a.yuen5678@gmail.com>
2025-09-25 15:12:30 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -126,7 +126,7 @@ resource "azurerm_linux_virtual_machine" "my_terraform_vm" {
 
   admin_ssh_key {
     username   = var.username
-    public_key = azapi_resource_action.ssh_public_key_gen.output.publicKey
+    public_key = file("~/.ssh/id_rsa.pub")
   }
 
   boot_diagnostics {
